9d343382cb86e8b6e2e40356ef40890241efaba7
Nullable <admin@nullable.se>
2012-05-23 01:48:36 +0200

    Added PK ID column to wrappers table, made pubkey TEXT

--- models.scala
+++ models.scala
@@ -10,9 +10,10 @@ object User extends Table("users") with PrimaryKey[Long] {
 	val pk = blid
 }
 
-object WrapperAccountMapping extends Table("wrappers") with PrimaryKey[String] {
+object WrapperAccountMapping extends Table("wrappers") with PrimaryKey[Long] {
+	val id = col[Long]("id")
 	val pubkey = col[String]("pubkey")
 	val blid = col[Long]("blid")
 
-	val pk = pubkey
+	val pk = id
 }
